4231fbe66b957f7dd134c98353bfdf665a77ddbd
Dmitry Koujelev <dkoujelev97@gmail.com>
2021-09-25 17:16:51 +0200

    more deps

--- packages/ui/src/Button.tsx
+++ packages/ui/src/Button.tsx
@@ -1,7 +1,12 @@
 import React from "react";
+import { getNiceMessage } from "@workspace/util";
 
 type ButtonProps = { text: string };
 
 export const Button: React.FC<ButtonProps> = ({ text }) => {
-  return <button>{text}</button>;
+  return (
+    <button>
+      {text} ({getNiceMessage()})
+    </button>
+  );
 };
